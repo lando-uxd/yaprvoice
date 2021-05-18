0d21b1a0b9f48d6eca2e44240f7fece9878f76c5
Orlando Guwee <orlandroide@gmail.com>
2021-05-18 10:46:46 -0700

    Add stuff

--- src/pages/PhoneConfirmation.js
+++ src/pages/PhoneConfirmation.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import PhoneInput from 'react-phone-number-input';
 import {Link} from 'react-router-dom'
 
 import style from '../style/phoneConfirm.module.css'
@@ -8,7 +9,13 @@ export default function PhoneConfirmation() {
   
   return (
     <div className={style.phoneConfirmContainer}>
-      <h2>Enter your phone number</h2>
+      <Link exact to="/" className={style.backBtn}>
+        <img src="" alt="" />
+      </Link>
+      <h2>Enter your phone number</h2>8
+      <PhoneInput international defaultCountry='US' value={value}>
+
+      </PhoneInput>
       <p>
        By entering your number you are agreeing to our terms {" "}
         <span>Terms of Service and Privacy Policy </span>
