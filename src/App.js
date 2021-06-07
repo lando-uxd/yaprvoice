33a53db5de07cdc3c5e61a1d5867ddb552fd0d9b
Jesse Lando F <orlandroide@gmail.com>
2021-06-06 23:29:36 -0700

    Test codesandbox

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import './App.css';
-import {BrowserRouter, Route} from 'react-router-dom'
+import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import React from "react";
 
-import Welcome from "./pages/Welcome"
-import PlanLayout from './pages/Layouts/PlanLayout'
-import PhoneConfirmation from './pages/PhoneConfirmation'
-import CodeConfirm from './pages/CodeConfirm'
+import Welcome from "./pages/Welcome";
+import PlanLayout from "./pages/Layouts/PlanLayout";
+import PhoneConfirmation from "./pages/PhoneConfirmation";
+import CodeConfirm from "./pages/CodeConfirm";
 
 function App() {
   return (
     <BrowserRouter>
       <PlanLayout>
-        <Route exact path='/' component={Welcome} />
-        <Route exact path='/invite' component={PhoneConfirmation} />
-        <Route exact path='/code_confirm' component={CodeConfirm} />
+        <Route exact path="/" component={Welcome} />
+        <Route exact path="/invite" component={PhoneConfirmation} />
+        <Route exact path="/code_confirm" component={CodeConfirm} />
       </PlanLayout>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
